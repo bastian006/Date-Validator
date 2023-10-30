606ddba6551863dc17ea31ff326a85ddf8c8eeff
bastian006 <bastiannazy11@gmail.com>
2023-10-30 00:08:10 -0300

    DV

---
+++ DateValidator/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DateValidator.Models;
+
+namespace DateValidator.Controllers;
+
+public class HomeController : Controller
+{
+[HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult HandleSubmission(Trip trip)
+        {
+
+            if (ModelState.IsValid)
+            { 
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
